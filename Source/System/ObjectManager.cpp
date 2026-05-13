23dc3b091b9838db6ca371cfe7025fb0cac90566
MDAIKI1230 <shimamizu1230@gmail.com>
2026-05-13 15:19:55 +0900

    必要なファイルを追加。

---
+++ Source/System/ObjectManager.cpp
@@ -0,0 +1,11 @@
+#include "ObjectManager.h"
+
+void ObjectManager::Update()
+{
+
+}
+
+void ObjectManager::FixedUpdate()
+{
+
+}
--- Source/System/ObjectManager.h
+++ Source/System/ObjectManager.h
@@ -1,1 +1,8 @@
 #pragma once
+
+class ObjectManager
+{
+public:
+	void Update();
+	void FixedUpdate();
+};
