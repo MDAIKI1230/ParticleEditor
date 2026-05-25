b717e52f413785e35a52a0162e4330769869b5d8
MDAIKI1230 <shimamizu1230@gmail.com>
2026-05-25 13:29:54 +0900

    AttributeBufferインターフェイス作成

---
+++ Source/System/VFX/Storage/Buffer/IAttributeBuffer.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "VFXConstants.h"
+
+class IAttributeBuffer
+{
+public:
+	// 仮想デストラクタ
+	virtual ~IAttributeBuffer() = 0;
+	// タイプ取得
+	virtual AttributeType GetType() = 0;
+	// GPUにデータを送る？
+	virtual void Upload() = 0;
+	// サイズ分生成
+	virtual void Resize(size_t _size) = 0;
+};
