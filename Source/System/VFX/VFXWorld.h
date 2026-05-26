e3e7cc40607939c3bd242a3cb4f0c351ae550b37
MDAIKI1230 <shimamizu1230@gmail.com>
2026-05-27 01:12:48 +0900

    VFXWorldちょい作り

--- Source/System/VFX/VFXWorld.h
+++ Source/System/VFX/VFXWorld.h
@@ -1,7 +1,21 @@
 #pragma once
 
-class VFXWorld
-{
-private:
+#include "IWorld.h"
 
+#include "Storage/VFXInstanceStorage.h"
+#include "System/BufferManager.h"
+#include "System/SimulatePipeline.h"
+
+class VFXWorld:public IWorld
+{
+public:
+	// コンストラクタ。
+	VFXWorld();
+	// 更新
+	void Update();
+	// 描画
+	void Draw();
+private:
+	BufferManager bufferManager;
+	SimulatePipeline simulatePipeline;
 };
